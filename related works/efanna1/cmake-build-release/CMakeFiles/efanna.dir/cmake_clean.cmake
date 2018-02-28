22890194e8cc76c74b4cdde4fd53d0eef0dd8ffa
markzxx <mark.zxx@qq.com>
2018-02-28 19:54:36 +0800

    cmakelist

---
+++ related works/efanna1/cmake-build-release/CMakeFiles/efanna.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/efanna.dir/samples/efanna_index_buildall.cc.obj"
+  "efanna.pdb"
+  "efanna.exe"
+  "efanna.exe.manifest"
+  "libefanna.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/efanna.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
