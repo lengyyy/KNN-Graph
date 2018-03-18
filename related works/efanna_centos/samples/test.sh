daef57364bdb460acf9614b4d09c50ef937724be
lengyyy <yue.leng@foxmail.com>
2018-03-18 19:06:51 +0800

    remove last version

--- related works/efanna_centos/samples/test.sh
+++
@@ -1,1 +0,0 @@
-./efanna_index_buildall_and_evaluate sift_base.fvecs sift.graph sift.trees 8 8 6 20 30 10 10 1 sift_graphtruth_100nn.ivecs 
--- related works/efanna_centos/samples/test_100n_build_smallsift.sh
+++
@@ -1,4 +0,0 @@
-./efanna_index_buildall_and_evaluate sift_base.fvecs sift.graph sift.trees 8 8 6 20 200 10 10 1 sift_graphtruth_100nn.ivecs
-./efanna_index_buildall_and_evaluate sift_base.fvecs sift.graph sift.trees 8 8 6 100 30 10 10 1 sift_graphtruth_100nn.ivecs
-./efanna_index_buildall_and_evaluate sift_base.fvecs sift.graph sift.trees 8 8 6 20 20 10 10 1 sift_graphtruth_100nn.ivecs
-./efanna_index_buildall_and_evaluate sift_base.fvecs sift.graph sift.trees 8 8 6 20 10 10 10 1 sift_graphtruth_100nn.ivecs
